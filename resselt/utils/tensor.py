8d7097ce011465f4fd618a92c1d1962d886ed139
umzi <ab5447612@gmail.com>
2024-07-06 17:21:17 +0300

    fix: 1 ch and add FixTile class

--- resselt/utils/tensor.py
+++ resselt/utils/tensor.py
@@ -17,7 +17,10 @@ def image2tensor(
         if img.dtype == np.uint8:
             img = img.astype(np.float32) / 255.0
 
-        tensor = torch.from_numpy(img.transpose(2, 0, 1))
+        if len(img.shape) == 2:
+            tensor = torch.from_numpy(img[None, ...])
+        else:
+            tensor = torch.from_numpy(img.transpose(2, 0, 1))
 
         if tensor.dtype != out_type:
             tensor = tensor.to(out_type)
