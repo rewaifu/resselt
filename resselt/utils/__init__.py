e9bcb20233664629546ea46b2061cacfd335eb16
umzi <ab5447612@gmail.com>
2024-07-06 22:37:00 +0300

    add: ATD

--- resselt/utils/__init__.py
+++ resselt/utils/__init__.py
@@ -1,5 +1,5 @@
 from .tiler import MaxTiler, AutoTiler, ExactTiler
-from .state_dict import canonicalize_state_dict, get_seq_len, pixelshuffle_scale, dysample_scale
+from .state_dict import canonicalize_state_dict, get_seq_len, pixelshuffle_scale, dysample_scale, get_pixelshuffle_params
 from .tensor import image2tensor, tensor2image, empty_cuda_cache
 from .upscaler import upscale_with_tiler
 
@@ -15,4 +15,5 @@ __all__ = [
     'ExactTiler',
     'pixelshuffle_scale',
     'dysample_scale',
+    'get_pixelshuffle_params',
 ]
