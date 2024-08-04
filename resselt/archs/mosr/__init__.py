47317873cc8e795a993661be37467cfb01422cb8
umzi <ab5447612@gmail.com>
2024-08-04 11:57:16 +0300

    add final mosr

--- resselt/archs/mosr/__init__.py
+++ resselt/archs/mosr/__init__.py
@@ -11,33 +11,40 @@ class MoSRArch(Architecture[MoSR]):
         super().__init__(
             id='MoSR',
             detect=KeyCondition.has_all(
-                'in_to_dim.weight',
-                'in_to_dim.bias',
-                'gblocks.0.gated.0.dccm.0.weight',
+                'gblocks.0.weight',
+                'gblocks.0.bias',
+                'gblocks.1.norm.weight',
+                'gblocks.1.norm.bias',
+                'gblocks.1.fc1.weight',
+                'gblocks.1.fc1.bias',
+                'gblocks.1.conv.weight',
+                'gblocks.1.conv.bias',
+                'gblocks.1.fc2.weight',
+                'gblocks.1.fc2.bias',
             ),
         )
 
     def load(self, state: Mapping[str, object]) -> WrappedModel:
-        len_blocks = get_seq_len(state, 'gblocks')
-        blocks = []
-        for i in range(len_blocks):
-            blocks.append(get_seq_len(state, 'gblocks.0.gated'))
-        dim = state['in_to_dim.weight'].shape[0]
-        in_ch = state['in_to_dim.weight'].shape[1]
-        expansion = state['gblocks.0.gated.0.gcnn.fc2.weight'].shape
-        expansion_ratio = expansion[1] / expansion[0]
-        if 'upsampler.weight' in state:
-            upsampler = 'conv'
-            upscale = 1
-            out_ch = state['upsampler.weight'].shape[0]
-        elif 'upsampler.0.weight' in state:
-            upsampler = 'ps'
-            out_ch = in_ch
-            upscale = pixelshuffle_scale(state['upsampler.0.weight'].shape[0], out_ch)
+        # Get values from state
+        n_block = get_seq_len(state, "gblocks") - 6
+        in_ch = state["gblocks.0.weight"].shape[1]
+        dim = state["gblocks.0.weight"].shape[0]
+
+        # Calculate expansion ratio and convolution ratio
+        expansion_ratio = (state["gblocks.1.fc1.weight"].shape[0] / state["gblocks.1.fc1.weight"].shape[1]) / 2
+        conv_ratio = state["gblocks.1.conv.weight"].shape[0] / dim
+        kernel_size = state["gblocks.1.conv.weight"].shape[2]
+        # Determine upsampler type and calculate upscale
+        if "upsampler.init_pos" in state:
+            upsampler = "dys"
+            out_ch = state["upsampler.end_conv.weight"].shape[0]
+            upscale = dysample_scale(state["upsampler.offset.weight"].shape[0])
         else:
-            upsampler = 'dys'
-            out_ch = state['upsampler.end_conv.weight'].shape[0]
-            upscale = dysample_scale(state['upsampler.offset.weight'].shape[0])
-        model = MoSR(in_ch=in_ch, out_ch=out_ch, upscale=upscale, blocks=blocks, dim=dim, upsampler=upsampler, expansion_ratio=expansion_ratio)
+            upsampler = "ps"
+            out_ch = in_ch
+            upscale = pixelshuffle_scale(state["upsampler.0.weight"].shape[0], out_ch)
+
+        model = MoSR(in_ch=in_ch, out_ch=out_ch, n_block=n_block, upscale=upscale, dim=dim, upsampler=upsampler,
+                     expansion_ratio=expansion_ratio, conv_ratio=conv_ratio, kernel_size=kernel_size)
 
         return WrappedModel(model=model, in_channels=in_ch, out_channels=out_ch, upscale=upscale, name='MoSR')
--- resselt/archs/mosr/arch.py
+++ resselt/archs/mosr/arch.py
@@ -1,183 +1,135 @@
 import torch
-
 from torch import nn
 from torch.nn.init import trunc_normal_
 
-
-# upscale, __ = net_opt()
-class DCCM(nn.Sequential):
-    "Doubled Convolutional Channel Mixer"
-
-    def __init__(self, dim: int):
-        super().__init__(
-            nn.Conv2d(dim, dim * 2, 3, 1, 1),
-            nn.Mish(),
-            nn.Conv2d(dim * 2, dim, 3, 1, 1),
-        )
-        trunc_normal_(self[-1].weight, std=0.02)
+from resselt.archs.utils import DySample
 
 
-class LKAT(nn.Module):
-    def __init__(self, n_feats):
+class LayerNorm(nn.Module):
+
+    def __init__(self, dim: int, eps: float = 1e-6):
         super().__init__()
-
-        self.conv0 = nn.Sequential(nn.Conv2d(n_feats, n_feats, 1, 1, 0), nn.GELU())
-        self.att = nn.Sequential(
-            nn.Conv2d(n_feats, n_feats, 7, 1, 7 // 2, groups=n_feats),
-            nn.Conv2d(
-                n_feats,
-                n_feats,
-                9,
-                stride=1,
-                padding=(9 // 2) * 3,
-                groups=n_feats,
-                dilation=3,
-            ),
-            nn.Conv2d(n_feats, n_feats, 1, 1, 0),
-        )
-
-        self.conv1 = nn.Conv2d(n_feats, n_feats, 1, 1, 0)
+        self.weight = nn.Parameter(torch.ones(dim))
+        self.bias = nn.Parameter(torch.zeros(dim))
+        self.eps = eps
 
     def forward(self, x):
-        x = self.conv0(x)
-        x = x * self.att(x)
-        return self.conv1(x)
+        u = x.mean(1, keepdim=True)
+        s = (x - u).pow(2).mean(1, keepdim=True)
+        x = (x - u) / torch.sqrt(s + self.eps)
+        return self.weight[:, None, None] * x + self.bias[:, None, None]
+
+
+class ConvBlock(nn.Module):
+    r"""https://github.com/joshyZhou/AST/blob/main/model.py#L22"""
+
+    def __init__(self, in_channel: int, out_channel: int, strides: int = 1):
+        super(ConvBlock, self).__init__()
+        self.strides = strides
+        self.in_channel = in_channel
+        self.out_channel = out_channel
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channel, out_channel, kernel_size=3, stride=strides, padding=1),
+            nn.Mish(),
+            nn.Conv2d(out_channel, out_channel, kernel_size=3, stride=strides, padding=1),
+            nn.Mish(),
+        )
+        self.conv11 = nn.Conv2d(in_channel, out_channel, kernel_size=1, stride=strides, padding=0)
+
+    def forward(self, x):
+        out1 = self.block(x)
+        out2 = self.conv11(x)
+        out = out1 + out2
+        return out
 
 
 class GatedCNNBlock(nn.Module):
-    r"""Our implementation of Gated CNN Block: https://arxiv.org/pdf/1612.08083
-    Args:
-        conv_ratio: control the number of channels to conduct depthwise convolution.
-            Conduct convolution on partial channels can improve paraitcal efficiency.
-            The idea of partial channels is from ShuffleNet V2 (https://arxiv.org/abs/1807.11164) and
-            also used by InceptionNeXt (https://arxiv.org/abs/2303.16900) and FasterNet (https://arxiv.org/abs/2303.03667)
+    r"""
+    modernized mambaout main unit
+    https://github.com/yuweihao/MambaOut/blob/main/models/mambaout.py#L119
     """
 
-    def __init__(self, dim, expansion_ratio=8 / 3, kernel_size=7, conv_ratio=1.0, drop_path=0.0):
+    def __init__(self, dim: int,
+                 expansion_ratio: float = 8 / 3,
+                 conv_ratio: float = 1.0,
+                 kernel_size: int = 7):
         super().__init__()
-        self.norm = nn.LayerNorm(dim, eps=1e-6)
+        self.norm = LayerNorm(dim)
         hidden = int(expansion_ratio * dim)
-        self.fc1 = nn.Linear(dim, hidden * 2)
+        self.fc1 = nn.Conv2d(dim, hidden * 2, 3, 1, 1)
+
         self.act = nn.Mish()
         conv_channels = int(conv_ratio * dim)
         self.split_indices = [hidden, hidden - conv_channels, conv_channels]
-        self.conv = nn.Conv2d(conv_channels, conv_channels, kernel_size=kernel_size, padding=kernel_size // 2, groups=conv_channels)
-        self.fc2 = nn.Linear(hidden, dim)
 
-    def forward(self, x):
-        shortcut = x  # [B, H, W, C]
-        x = self.norm(x)
-        g, i, c = torch.split(self.fc1(x), self.split_indices, dim=-1)
-        c = c.permute(0, 3, 1, 2)  # [B, H, W, C] -> [B, C, H, W]
-        c = self.conv(c)
-        c = c.permute(0, 2, 3, 1)  # [B, C, H, W] -> [B, H, W, C]
-        x = self.fc2(self.act(g) * torch.cat((i, c), dim=-1))
-        return x + shortcut
+        self.conv = nn.Conv2d(conv_channels, conv_channels, kernel_size, 1, kernel_size // 2, groups=conv_channels)
+        self.fc2 = nn.Conv2d(hidden, dim, 3, 1, 1)
+        self.apply(self._init_weights)
 
-
-class GatedBlocks(nn.Module):
-    def __init__(self, dim, drop_path, expansion_ratio):
-        super().__init__()
-        self.dccm = DCCM(dim)
-        self.gcnn = GatedCNNBlock(dim, expansion_ratio=expansion_ratio, drop_path=drop_path)
-        # self.cab =LKAT(dim)
-
-    def forward(self, x):
-        x = self.dccm(x)
-        x = self.gcnn(x.permute(0, 2, 3, 1)).permute(0, 3, 1, 2)
-        # x = self.cab(x)
-        return x
-
-
-class GBlocks(nn.Module):
-    def __init__(self, n_blocks, dim, drop_path, expansion_ratio):
-        super().__init__()
-        self.gated = nn.Sequential(*[GatedBlocks(dim, drop_path, expansion_ratio) for _ in range(n_blocks)])
-        self.end = LKAT(dim)
+    @staticmethod
+    def _init_weights(m):
+        if isinstance(m, nn.Conv2d | nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
         shortcut = x
-        x = self.gated(x)
-        return self.end(x) + shortcut
+        x = self.norm(x)
+        g, i, c = torch.split(self.fc1(x), self.split_indices, dim=1)
+        c = self.conv(c)
+        x = self.act(self.fc2(self.act(g) * torch.cat((i, c), dim=1)))
+        return x + (shortcut - 0.5)
 
 
 class mosr(nn.Module):
-    def __init__(
-        self,
-        in_ch: int = 3,
-        out_ch: int = 3,
-        upscale: int = 2,
-        blocks: list[int] = [5],
-        dim: int = 48,
-        upsampler: str = 'ps',
-        drop_path: float = 0.0,
-        is_lite: bool = False,
-        expansion_ratio: float = 1.5,
-    ):
+    """Mamba Out Super-Resolution"""
+
+    def __init__(self,
+                 in_ch: int = 3,
+                 out_ch: int = 3,
+                 upscale: int = 4,
+                 n_block: int = 24,
+                 dim: int = 64,
+                 upsampler: str = "ps",  # "ps" "ds"
+                 kernel_size: int = 7,
+                 expansion_ratio: float = 1.5,
+                 conv_ratio: float = 1.0
+                 ):
         super(mosr, self).__init__()
+        if upsampler in ["ps"]:
+            out_ch = in_ch
+        self.gblocks = nn.Sequential(*[nn.Conv2d(in_ch, dim, 3, 1, 1)] +
+                                      [GatedCNNBlock(dim=dim,
+                                                     expansion_ratio=expansion_ratio,
+                                                     kernel_size=kernel_size,
+                                                     conv_ratio=conv_ratio,
 
-        # dp_rates = [x.item() for x in torch.linspace(0, drop_path, blocks)]
-        self.in_to_dim = nn.Conv2d(in_ch, dim, 3, 1, 1)
-        self.gblocks = nn.Sequential(*[GBlocks(block, dim, 0, expansion_ratio=expansion_ratio) for block in blocks])
-        if upsampler == 'ps':
-            self.upsampler = nn.Sequential(nn.Conv2d(dim, out_ch * (upscale**2), 3, padding=1), nn.PixelShuffle(upscale))
-            # trunc_normal_(self.upsampler[0].weight, std=0.02)
-        elif upsampler == 'dys':
-            # self.upsampler = DySample(dim, out_ch, upscale)
-            pass
-        elif upsampler == 'conv':
-            if upsampler != 1:
-                msg = 'conv supports only 1x'
-                raise ValueError(msg)
+                                                     )
+                                       for _ in range(n_block)] +
+                                      [nn.Conv2d(dim, dim * 2, 3, 1, 1),
+                                       nn.Mish(),
+                                       nn.Conv2d(dim * 2, dim, 3, 1, 1),
+                                       nn.Mish(),
+                                       nn.Conv2d(dim, dim, 1, 1)]
+                                     )
 
-            self.upsampler = nn.Conv2d(dim, out_ch, 3, padding=1)
+        self.shortcut = ConvBlock(in_ch, dim)
+
+        if upsampler == "ps":
+            self.upsampler = nn.Sequential(
+                nn.Conv2d(dim, out_ch * (upscale ** 2), 3, 1, 1),
+                nn.PixelShuffle(upscale)
+            )
+        elif upsampler == "dys":
+            self.upsampler = DySample(dim, out_ch, upscale)
         else:
             raise NotImplementedError(
                 f'upsampler: {upsampler} not supported, choose one of these options: \
-                ["ps", "dys", "conv"] conv supports only 1x'
-            )
+                ["ps", "dys"]')
 
     def forward(self, x):
-        x = self.in_to_dim(x)
-        x = self.gblocks(x)
+        x = self.gblocks(x) + (self.shortcut(x) - 0.5)
         return self.upsampler(x)
 
 
-if __name__ == '__main__':
-    import os
-    import sys
-
-    sys.path.append(os.path.abspath('.'))
-    upscale = 4
-    height = 256
-    width = 256
-    import os
-
-    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-    model = mosr().cuda()
-
-    params = sum(map(lambda x: x.numel(), model.parameters()))
-    results = dict()
-    results['runtime'] = []
-    model.eval()
-
-    x = torch.randn((1, 3, height, width)).cuda()
-    start = torch.cuda.Event(enable_timing=True)
-    end = torch.cuda.Event(enable_timing=True)
-
-    with torch.no_grad():
-        # x = model(x)
-        for _ in range(10):
-            x_sr = model(x)
-        for _ in range(100):
-            start.record()
-            x_sr = model(x)
-            end.record()
-            torch.cuda.synchronize()
-            results['runtime'].append(start.elapsed_time(end))  # milliseconds
-    print(x.shape)
-
-    print('{:.2f}ms'.format(sum(results['runtime']) / len(results['runtime'])))
-    results['memory'] = torch.cuda.max_memory_allocated(torch.cuda.current_device()) / 1024**2
-    print('Max Memery:{:.2f}[M]'.format(results['memory']))
-    print(f'Height:{height}->{x_sr.shape[2]}\nWidth:{width}->{x_sr.shape[3]}\nParameters:{params / 1e3:.2f}K')
